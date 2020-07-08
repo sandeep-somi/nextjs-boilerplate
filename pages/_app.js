42b520e39809601b94c0cbaa4d0ce4c0d091f6d7
Sandeep <soami.ssr@gmail.com>
2020-07-09 02:31:53 +0530

    removed get initia props

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import '../styles/index.scss';
 import { appWithTranslation } from '../i18n';
+// import App from 'next/app';
 import store from '../redux/store'
+
+// class AppContainer extends App {
+
+//   static async getInitialProps({ Component, ctx }) {
+//     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+//     return { pageProps }
+//   }
+
+//   render() {
+//     const { Component, pageProps } = this.props;
+//     return <Component {...pageProps} />
+//   }
+// }
 
 function AppContainer({ Component, pageProps }) {
 
@@ -10,11 +24,11 @@ function AppContainer({ Component, pageProps }) {
   )
 }
 
-AppContainer.getInitialProps = async ({ Component, ctx }) => {
-  const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-  return {
-    pageProps
-  }
-}
+// AppContainer.getInitialProps = async ({ Component, ctx }) => {
+//   const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+//   return {
+//     pageProps
+//   }
+// }
 
 export default store.withRedux(appWithTranslation(AppContainer));
